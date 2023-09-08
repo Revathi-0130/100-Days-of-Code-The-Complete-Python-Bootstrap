4a3f1971444736452844f7dcc1c96b999b946eb2
Revathi-0130 <138806730+Revathi-0130@users.noreply.github.com>
2023-09-09 00:15:48 +0530

    Create Day_1_code.py

---
+++ Day_1_code.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator") # greetings
+city = input("What is the city you grew up in?\n") #getting the city name
+pet_name = input("What is your pet name?\n") #getting the name of the pet
+band_name = city + " " + pet_name # concatenating two strings
+print("the band name generated is:",band_name) #printing the band name

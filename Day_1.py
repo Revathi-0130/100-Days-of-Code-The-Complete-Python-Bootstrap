31a5659da6cdd2b108dd16b33dbedb2a2401a340
Revathi-0130 <138806730+Revathi-0130@users.noreply.github.com>
2023-09-09 00:31:26 +0530

    Update and rename Day_1_code.py to Day_1.py

--- Day_1.py
+++ Day_1.py
@@ -1,3 +1,5 @@
+# Python code for band name generator
+
 print("Welcome to the band name generator") # greetings
 city = input("What is the city you grew up in?\n") #getting the city name
 pet_name = input("What is your pet name?\n") #getting the name of the pet
